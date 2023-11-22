008dd51fe24766f3907370e0cc277b755074356c
TemRevil <temrevil@gmail.com>
2023-11-22 22:05:08 +0200

    Edit On Information Cards
    Add More 3 Photos
    There Was An Errors In Nav Bar I'm Not Sure How But I Tried Many Ways To Keep It Good As I Can
    By Change Bootstrap Files And Remove The Old One

--- KyGen NewVer/JavaScripts/Footer.js
+++ KyGen NewVer/JavaScripts/Footer.js
@@ -1,74 +1,9 @@
-const footer = document.getElementById('main-footer');
-const AboutUsButton = document.getElementById('AboutUsButton');
-const kygenAboutUs = document.getElementById('KyGenAboutUs');
-const kygenVersion = document.getElementById('KyGenVersion');
-
-let isFooterRaised = false;
-
-AboutUsButton.addEventListener('click', function (e) {
-    e.preventDefault();
-    
-    if (isFooterRaised) {
-        // إذا كان ال footer في حالة مرفوعة، قم بإعادته للوضع العادي
-        footer.style.transform = 'translateY(0%)';
-        isFooterRaised = false;
-        KyGenAboutUsDiv.style.display = 'none';
-        VerticalLine.style.display = 'none';
-        ImgAboutUs.style.display = 'none';
-        LiveAboutUs.style.display = 'none';
-        ID.style.display = 'none';
-        NoIdeasDiv.style.display = 'none';
-        //----------------------------------------
-        FeedbackForm.style.display = 'none';
-        LockLapDiv.style.display = 'none';
-    } else {
-        // إذا كان ال footer في حالة عادية، قم برفعه للأعلى
-        footer.style.transform = 'translateY(-60%)';
-        isFooterRaised = true;
-        KyGenAboutUsDiv.style.display = 'block'; // إظهار العنصر عند الضغط على About Us
-        kygenTitle.style.transform = 'translateY(-60%)';
-        kygenVersion.style.display = 'block'; // إظهار العنصر عند الضغط على About Us
-        VerticalLine.style.display = 'block'; // إظهار العنصر عند الضغط على About Us
-        ImgAboutUs.style.display = 'block'; // إظهار العنصر عند الضغط على About Us
-        LiveAboutUs.style.display = 'block';
-        ID.style.display = 'block'; // إظهار العنصر عند الضغط على About Us
-        NoIdeasDiv.style.display = 'block'; // إظهار العنصر عند الضغط على About Us
-    }
-});
-
-//--------------------------------------------------------------------------------------------
-
-const FeedbackButton = document.getElementById('FeedbackButton');
-
-FeedbackButton.addEventListener('click', function (e) {
-    e.preventDefault();
-    
-    if (isFooterRaised) {
-        // إذا كان ال footer في حالة مرفوعة، قم بإعادته للوضع العادي
-        footer.style.transform = 'translateY(0%)';
-        isFooterRaised = false;
-        KyGenAboutUsDiv.style.display = 'none';
-        VerticalLine.style.display = 'none';
-        ImgAboutUs.style.display = 'none';
-        LiveAboutUs.style.display = 'none';
-        ID.style.display = 'none';
-        NoIdeasDiv.style.display = 'none';
-        FeedbackForm.style.display = 'none';
-        LockLapDiv.style.display = 'none';
-    } else {
-        // إذا كان ال footer في حالة عادية، قم برفعه للأعلى
-        footer.style.transform = 'translateY(-60%)';
-        isFooterRaised = true;
-        FeedbackForm.style.display = 'block';
-        LockLapDiv.style.display = 'block';
-    }
-});
 // استهدف عناصر الراديو والمربعات النصية والتحقق من التغيير
-const feedbackRadio = document.getElementById("FeedbackRadioL");
-const contactRadio = document.getElementById("ContactRadioL");
-const feedbackForm = document.getElementById("FeedbackForm");
-const emailInput = document.getElementById("Email");
-const numberInput = document.getElementById("Number");
+const feedbackRadio = document.getElementById("h-footer-form-feedback");
+const contactRadio = document.getElementById("h-footer-form-contact");
+const feedbackForm = document.getElementById("h-footer-form");
+const emailInput = document.getElementById("h-footer-form-email");
+const numberInput = document.getElementById("h-footer-form-number");
 
 feedbackRadio.addEventListener("change", function () {
     if (feedbackRadio.checked) {
@@ -91,6 +26,5 @@ contactRadio.addEventListener("change", function () {
         emailInput.removeAttribute("required");
     }
 });
-
 
 //--------------------------------------------------------------------------------------------
