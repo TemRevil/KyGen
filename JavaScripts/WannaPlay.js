7cb920047f8260f4efabedc28e84cea61e3b72f9
Tem Revil <temrevil@gmail.com>
2024-01-09 17:57:19 +0200

    WannaPlay
    - Script Update

--- JavaScripts/WannaPlay.js
+++ JavaScripts/WannaPlay.js
@@ -1,11 +1,33 @@
 const passwordInput = document.querySelector('.p-playpass');
 const rule1 = document.querySelector('.p-rule-box.r1');
 const rule2 = document.querySelector('.p-rule-box.r2');
+const surrenderButton = document.querySelector('.p-playpass-surrender');
+const surrenderMessage = document.querySelector('.p-surrender-message');
+const copyButton = document.querySelector('.p-playpass-copy');
+const copyMessage = document.querySelector('.p-copy-message');
+const winnerMessage = document.querySelector('.p-winner-message');
 
-// حدث عند كتابة أي شيء في الحقل
+
 passwordInput.addEventListener('input', function () {
     validateRule1();
     validateRule2();
+    checkAllRules();
+});
+
+surrenderButton.addEventListener('click', function () {
+    passwordInput.disabled = true;
+    surrenderMessage.classList.remove('p-display');
+});
+
+copyButton.addEventListener('click', function () {
+    passwordInput.select();
+    document.execCommand('copy');
+
+    copyMessage.classList.remove('p-display');
+    
+    setTimeout(function () {
+        copyMessage.classList.add('p-display');
+    }, 2000);
 });
 
 function validateRule1() {
@@ -24,9 +46,18 @@ function validateRule1() {
 }
 
 function validateRule2() {
-    const numbersCount = (passwordInput.value.match(/\d/g) || []).length; if (numbersCount >= 3) {
+    const numbersCount = (passwordInput.value.match(/\d/g) || []).length;
+    if (numbersCount >= 3) {
         rule2.classList.add('active');
     } else {
         rule2.classList.remove('active');
+    }
+}
+
+function checkAllRules() {
+    if (rule1.classList.contains('active') && rule2.classList.contains('active')) {
+        winnerMessage.classList.remove('p-display');
+    } else {
+        winnerMessage.classList.add('p-display');
     }
 }
