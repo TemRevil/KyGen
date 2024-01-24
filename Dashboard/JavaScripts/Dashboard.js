54cbeddc0ded9967dd6dddf5e1cc802e7d6cc0c6
Tem Revil <temrevil@gmail.com>
2024-01-24 12:39:08 +0200

    Home

--- Dashboard/JavaScripts/Dashboard.js
+++ Dashboard/JavaScripts/Dashboard.js
@@ -87,6 +87,59 @@ var options = {
 var chart = new ApexCharts(document.querySelector("#PerDay"), options);
 chart.render();
 
+// Work Chart
+var options = {
+  chart: {
+    type: 'area',
+    height: 350
+  },
+  series: [
+    {
+      name: 'Generator',
+      data: [124, 529, 789, 987, 1004, 1217, 1241, 1148, 987, 1324, 1471, 1134],
+      curve: 'smooth'
+    },
+    {
+      name: 'WannaPlay',
+      data: [17, 20, 21, 12, 17, 8, 48, 2, 15, 17, 31, 67],
+      curve: 'smooth'
+    },
+    {
+      name: 'Wordlist',
+      data: [121, 202, 44, 12, 54, 87, 247, 144, 389, 121, 341, 674],
+      curve: 'smooth'
+    },
+    {
+      name: 'Ryan Ai',
+      data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      curve: 'smooth'
+    }
+  ],
+  xaxis: {
+    categories: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+  },
+  tooltip: {
+    y: {
+      formatter: function (value) {
+        return value + " units";
+      }
+    },
+    theme: 'light',
+    marker: {
+      show: true,
+    }
+  },
+  legend: {
+    show: true,
+    position: 'top',
+    onItemClick: {
+      toggleDataSeries: false
+    },
+  }
+}
+var chart = new ApexCharts(document.querySelector("#Work"), options);
+chart.render();
+
 // Langs Users Chart
 var options = {
   chart: {
--- README.md
+++
@@ -1,6 +0,0 @@
-# Encryptor
-Website Specialized in Multi Security Pass Ways
-Still In Update
-Want to Join Our Team Call Us On 
-      +201001308280
- or temrevil@gmail.com
