b41a67526118ada4185da5457dd4d11e270f00f5
Tem Revil <temrevil@gmail.com>
2024-01-24 03:05:26 +0200

    Home
    - Update Login Box For All

--- JavaScripts/Login.js
+++ JavaScripts/Login.js
@@ -1,33 +1,34 @@
-const loginHeaderButton = document.getElementById('login-header-button');
-const loginContainer = document.getElementById('login-container');
-const loginOpacity = document.getElementById('LoginOpacity');
-const loginForm = document.getElementById('login-form');
-
 document.addEventListener('DOMContentLoaded', function () {
-    const usernameInput = document.getElementById('username');
-    const passwordInput = document.getElementById('password');
-    const errorMessage = document.getElementById('error-message');
-    const successMessage = document.getElementById('success-message');
+    const loginForm = document.getElementById('login-form');
 
-    usernameInput.value = '';
-    passwordInput.value = '';
-    successMessage.textContent = '';
-    errorMessage.textContent = '';
+    if (loginForm) {
+        const usernameInput = document.getElementById('username');
+        const passwordInput = document.getElementById('password');
+        const errorMessage = document.getElementById('error-message');
+        const successMessage = document.getElementById('success-message');
 
-    loginForm.addEventListener('submit', function (e) {
-        e.preventDefault();
-
-        const username = usernameInput.value;
-        const password = passwordInput.value;
-
-        errorMessage.textContent = '';
+        usernameInput.value = '';
+        passwordInput.value = '';
         successMessage.textContent = '';
+        errorMessage.textContent = '';
 
-        if (username === 'Revil' && password === 'Revil21') {
-            successMessage.textContent = 'Welcome Mr.Revil!';
-            window.location.href = 'Dashboard/Dashboard.html';
-        } else {
-            errorMessage.textContent = 'Invalid username or password.';
-        }
-    });
+        loginForm.addEventListener('submit', function (e) {
+            e.preventDefault();
+
+            const username = usernameInput.value;
+            const password = passwordInput.value;
+
+            errorMessage.textContent = '';
+            successMessage.textContent = '';
+
+            if (username === 'Revil' && password === 'Revil21') {
+                successMessage.textContent = 'Welcome Mr.Revil!';
+                window.location.href = 'Dashboard/Dashboard.html';
+            } else {
+                errorMessage.textContent = 'Invalid username or password.';
+            }
+        });
+    } else {
+        console.error('Login form not found in the HTML.');
+    }
 });
